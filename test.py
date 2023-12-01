c8fb36eb6b059fa00403f095dfa67193cb6d3a53
honggom <jaehongbyeon98@gmail.com>
2023-12-01 12:50:25 +0900

    :memo: 백준 1904 01타일 풀이

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-import sys
-input = sys.stdin.readline
+print((15746 + 15748)  % 15746)
 
-n = int(input())
-dp = [0] * (10**6+1)
-
-for i in range(2, n + 1):
-    dp[i] = dp[i - 1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[int(i / 2)] + 1, dp[i])
-    if i % 3 == 0:
-        dp[i] = min(dp[int(i / 3)] + 1, dp[i])
-
-print(dp[n])
