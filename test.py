8ebef5a20189b3fc7eb7411a5a89eea4bdd81c7e
honggom <jaehongbyeon98@gmail.com>
2023-12-04 11:34:00 +0900

    :memo: 백준 1021 회전하는 큐 풀이

--- test.py
+++ test.py
@@ -1,18 +1,7 @@
-N, M = map(int, input().split())
-tree = list(map(int, input().split()))
-start, end = 1, max(tree)  # 이분탐색 검색 범위 설정
+from collections import deque
 
-while start <= end:  # 적절한 벌목 높이를 찾는 알고리즘
-    mid = (start + end) // 2
+q = deque([1,2,3,4,5])
 
-    log = 0  # 벌목된 나무 총합
-    for i in tree:
-        if i >= mid:
-            log += i - mid
+q.rotate(-1)
 
-    # 벌목 높이를 이분탐색
-    if log >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
-print(end)
+print(q)
