d53d76e5cfa51f329c12b0e97a0ee81e56e1c76b
honggom <jaehongbyeon98@gmail.com>
2023-12-25 23:03:00 +0900

    :memo: 백준 9465 스티커 풀이

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
-from copy import deepcopy
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    n = int(input())
+    dp = [[0] * 1000000 for _ in range(2)]
+    for i in range(2):
+        nums = list(map(int, input().split()))
+        for j in range(n):
+            dp[i][j] = nums[j]
+    dp[0][1] = dp[1][0] + dp[0][1]
+    dp[1][1] = dp[0][0] + dp[1][1]
 
+    for i in range(2, n):
+        dp[0][i] = dp[0][i] + max(dp[0][i - 2], dp[1][i - 1], dp[1][i - 2])
+        dp[1][i] = dp[1][i] + max(dp[0][i - 1], dp[0][i - 2], dp[1][i - 2])
 
-def solution(t):
-    dp = deepcopy(t)
-
-    for i in range(1, len(t)):
-        for j in range(len(t[i])):
-            if j == 0:
-                dp[i][j] += dp[i - 1][j]
-            elif j == len(t[i]) - 1:
-                dp[i][j] += dp[i - 1][j - 1]
-            else:
-                dp[i][j] = max(dp[i - 1][j] + dp[i][j], dp[i - 1][j - 1] + dp[i][j])
-
-    return max(dp[-1])
+    print(max(dp[0][n - 1], dp[1][n - 1]))
